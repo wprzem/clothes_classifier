738a4dbca599000f4ed077f47bdd05097c3445aa
pw <wozniak.przem@gmail.com>
2021-03-29 15:25:49 +0200

    command line argument parser

--- clothes_classifier.py
+++ clothes_classifier.py
@@ -1,23 +1,29 @@
 import cv2
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import numpy as np
+import argparse
 
 
 def img_from_url(url):
-    resp = urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
+    try:
+        resp = urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        return image
+    except HTTPError:
+        print('Invalid url - image not found')
 
 
 def main():
-    url = 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/chhclityponr312bcvdt/aerobill-legacy91-training-hat-1TrTBz.jpg'
-
-    img = img_from_url(url)
-    if img is None:
-        print('Invalid url - image not found')
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', dest='url', required=True, help='url to the image')
+    args = parser.parse_args()
+    
+    img = img_from_url(args.url)
+    if img is not None:
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
 
 
 if __name__ == '__main__':
